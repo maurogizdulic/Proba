1f212c008264cbdad8604a24b8e0c3d78282b1d6
Mauro Gizdulic <mauro.gizdulic@gmail.com>
2023-03-07 14:22:13 +0100

    Adding new class for entry id, name and surname.

---
+++ src/UpisKandidataISudaca.java
@@ -0,0 +1,10 @@
+public class UpisKandidataISudaca {
+    Sudac s1 = new Sudac(1, "Mirko", "Mirkic");
+    Sudac s2 = new Sudac(2, "Slavko", "Slavkic");
+    Sudac s3 = new Sudac(3, "Pero", "Peric");
+
+    Natjecatelj n1 = new Natjecatelj(1, "Marta", "Martic");
+    Natjecatelj n2 = new Natjecatelj(2, "Sara", "Saric");
+    Natjecatelj n3 = new Natjecatelj(3, "Stela", "Stelic");
+    Natjecatelj n4 = new Natjecatelj(4, "Nina", "Ninic");
+}
